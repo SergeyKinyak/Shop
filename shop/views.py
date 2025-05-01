429782a6a27043dc31c94197b39c3099a9e6495f
SergeyKinyak <kinyaksereja@gmail.com>
2025-05-01 18:44:40 +0300

    table of courses info

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Course
 
 
-
-
 def index(request):
-	courses=Course.objects.all()
-	return HttpResponse(''.join([str(course) + '<br>' for course in courses]))
+    courses = Course.objects.all()
+    return render(request, 'courses.html',{'courses':courses})
