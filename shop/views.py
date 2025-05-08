388868138a4b7889469b5af15ffe173dbeaf0c53
SergeyKinyak <kinyaksereja@gmail.com>
2025-05-08 16:08:42 +0300

    routing courses

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html',{'courses':courses})
+    return render(request, 'courses.html', {'courses': courses})
+
+
+def single_course(request, course_id):
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'single_course.html', {'course': course})
