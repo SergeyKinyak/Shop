8c9c73d49417f511e796f2d498b2793ff7b8583f
SergeyKinyak <kinyaksereja@gmail.com>
2025-05-12 17:09:27 +0300

    post methods,api title upper

--- api/models.py
+++ api/models.py
@@ -18,3 +18,15 @@ class CourseResource(ModelResource):
         allowed_method = ['get', 'post', 'delete']
         authentication = CustomAuthentication()
         authorization = Authorization()
+
+    def hydrate(self, bundle):
+        bundle.obj.category_id = bundle.data['category_id']
+        return bundle
+
+    def dehydrate(self, bundle):
+        bundle.data['category_id'] = bundle.obj.category
+        return bundle
+
+    def dehydrate_title(self, bundle):
+        return bundle.data['title'].upper()
+        
